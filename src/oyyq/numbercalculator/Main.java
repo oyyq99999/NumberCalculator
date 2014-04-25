fe7962931895908050d697a766e95ab8a595faa3
Yunqi Ouyang <oyyq9999999@163.com>
2014-04-25 22:39:14 +0800

    format code

--- src/oyyq/numbercalculator/Main.java
+++ src/oyyq/numbercalculator/Main.java
@@ -1,56 +1,89 @@
 package oyyq.numbercalculator;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import oyyq.numbercalculator.datastructure.Expression;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		NumberCalculator calculator = new NumberCalculator(new Number[] { 4, 11, 11, 12, 13 }, 24);
-		Number[] numbers = calculator.getNumbers();
-		for (Number number : numbers) {
-			System.out.print(number + " ");
-		}
-		System.out.println();
-		// System.err.close();
-		long start = System.currentTimeMillis();
-		System.out.println(calculator.calculate(true));
-		System.err.println(System.currentTimeMillis() - start);
-		ArrayList<Expression> results = calculator.getResults();
-		System.out.println(results.size());
-		for (Expression result : results) {
-			System.out.println(result.toString());
-		}
-		System.exit(0);
-		int solveCount = 0;
-		int solvableCount = 0;
-		int maxSolves = 0;
-		for (int i = 1; i <= 13; i++) {
-			for (int j = i; j <= 13; j++) {
-				for (int k = j; k <= 13; k++) {
-					for (int l = k; l <= 13; l++) {
-						NumberCalculator calc = new NumberCalculator(new Number[] { i, j, k, l });
-						System.out.println(i + " " + j + " " + k + " " + l);
-						System.out.println(calc.calculate(true));
-						results = calc.getResults();
-						System.out.println(results.size());
-						solveCount += results.size();
-						if (results.size() > 0) {
-							solvableCount++;
-						}
-						if (results.size() > maxSolves) {
-							maxSolves = results.size();
-						}
-						for (Expression result : results) {
-							System.out.println(result);
-						}
-					}
-				}
-			}
-		}
-		System.out.println(solvableCount + " " + solveCount + " " + maxSolves);
-	}
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        // int odd;
+        // int even;
+        // int max = -1;
+        // for (int number = 1; number <= 100; number++) {
+        // odd = -1;
+        // even = -1;
+        // System.out.println(number);
+        // for (int count = 1;; count++) {
+        // Number[] numbers = new Number[count];
+        // Arrays.fill(numbers, number);
+        // NumberCalculator calc = new NumberCalculator(numbers);
+        // if (calc.calculate(false)) {
+        // if ((count & 1) == 0) {
+        // even = count;
+        // } else {
+        // odd = count;
+        // }
+        // }
+        // if (even > 0 && odd > 0) {
+        // if (Math.abs(even - odd) != 1) {
+        // System.err.println("Failed at:" + number + ": " + even + " " + odd);
+        // System.exit(1);
+        // }
+        // if (Math.min(even, odd) > max) {
+        // max = Math.min(even, odd);
+        // }
+        // break;
+        // }
+        // }
+        // }
+        // System.out.println(max);
+        // System.exit(0);
+        NumberCalculator calculator = new NumberCalculator(new Number[] {1, 5, 5, 7, 13}, 24);
+        Number[] numbers = calculator.getNumbers();
+        for (Number number : numbers) {
+            System.out.print(number + " ");
+        }
+        System.out.println();
+        // System.err.close();
+        long start = System.currentTimeMillis();
+        System.out.println(calculator.calculate(true));
+        System.err.println(System.currentTimeMillis() - start);
+        ArrayList<Expression> results = calculator.getResults();
+        System.out.println(results.size());
+        for (Expression result : results) {
+            System.out.println(result.toString());
+        }
+        System.exit(0);
+        int solveCount = 0;
+        int solvableCount = 0;
+        int maxSolves = 0;
+        for (int i = 1; i <= 13; i++) {
+            for (int j = i; j <= 13; j++) {
+                for (int k = j; k <= 13; k++) {
+                    for (int l = k; l <= 13; l++) {
+                        NumberCalculator calc = new NumberCalculator(new Number[] {i, j, k, l});
+                        System.out.println(i + " " + j + " " + k + " " + l);
+                        System.out.println(calc.calculate(true));
+                        results = calc.getResults();
+                        System.out.println(results.size());
+                        solveCount += results.size();
+                        if (results.size() > 0) {
+                            solvableCount++;
+                        }
+                        if (results.size() > maxSolves) {
+                            maxSolves = results.size();
+                        }
+                        for (Expression result : results) {
+                            System.out.println(result);
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(solvableCount + " " + solveCount + " " + maxSolves);
+    }
 
 }
--- src/oyyq/numbercalculator/datastructure/Expression.java
+++ src/oyyq/numbercalculator/datastructure/Expression.java
@@ -3,123 +3,129 @@ package oyyq.numbercalculator.datastructure;
 import com.github.kiprobinson.util.BigFraction;
 
 public class Expression {
-	private Expression operand1;
-	private Expression operand2;
-	private Operator operator;
-	private boolean simple;
-	private int totalOperatorCount;
-	private BigFraction value;
 
-	private static BigFraction calcValue(Expression op1, Operator operator, Expression op2) {
-		BigFraction result = null;
-		switch (operator) {
-		case PLUS:
-			result = op1.value.add(op2.value);
-			break;
-		case MINUS:
-			result = op1.value.subtract(op2.value);
-			break;
-		case MULTIPLY:
-			result = op1.value.multiply(op2.value);
-			break;
-		case DIVIDE:
-			if (op2.value.equalsNumber(0)) {
-				throw new ArithmeticException("Divide by zero");
-			}
-			result = op1.value.divide(op2.value);
-			break;
-		default:
-			throw new IllegalArgumentException("Illegal operator");
-		}
-		return result;
-	}
+    private Expression  operand1;
+    private Expression  operand2;
+    private Operator    operator;
+    private boolean     simple;
+    private int         totalOperatorCount;
+    private BigFraction value;
 
-	public Expression(Expression operand1, Operator operator, Expression operand2) {
-		if (operand1 == null || operand2 == null || operator == null) {
-			throw new IllegalArgumentException("Null parameter");
-		}
-		this.operand1 = operand1;
-		this.operand2 = operand2;
-		this.operator = operator;
-		value = calcValue(operand1, operator, operand2);
-		simple = false;
-		totalOperatorCount = operand1.totalOperatorCount + operand2.totalOperatorCount;
-	}
+    private static BigFraction calcValue(Expression op1, Operator operator, Expression op2) {
+        BigFraction result = null;
+        switch (operator) {
+            case PLUS:
+                result = op1.value.add(op2.value);
+                break;
+            case MINUS:
+                result = op1.value.subtract(op2.value);
+                break;
+            case MULTIPLY:
+                result = op1.value.multiply(op2.value);
+                break;
+            case DIVIDE:
+                if (op2.value.equalsNumber(0)) {
+                    throw new ArithmeticException("Divide by zero");
+                }
+                result = op1.value.divide(op2.value);
+                break;
+            default:
+                throw new IllegalArgumentException("Illegal operator");
+        }
+        return result;
+    }
 
-	public Expression(Number value) {
-		this.value = BigFraction.valueOf(value);
-		operand1 = operand2 = null;
-		operator = null;
-		simple = true;
-		totalOperatorCount = 1;
-	}
+    public Expression(Expression operand1, Operator operator, Expression operand2) {
+        if (operand1 == null || operand2 == null || operator == null) {
+            throw new IllegalArgumentException("Null parameter");
+        }
+        this.operand1 = operand1;
+        this.operand2 = operand2;
+        this.operator = operator;
+        value = calcValue(operand1, operator, operand2);
+        simple = false;
+        totalOperatorCount = operand1.totalOperatorCount + operand2.totalOperatorCount;
+    }
 
-	public Expression(Number operand1, Operator operator, Number operand2) {
-		this(new Expression(operand1), operator, new Expression(operand2));
-	}
+    public Expression(Number value) {
+        this.value = BigFraction.valueOf(value);
+        operand1 = operand2 = null;
+        operator = null;
+        simple = true;
+        totalOperatorCount = 1;
+    }
 
-	public Expression getOperand1() {
-		return operand1;
-	}
+    public Expression(Number operand1, Operator operator, Number operand2) {
+        this(new Expression(operand1), operator, new Expression(operand2));
+    }
 
-	public Expression getOperand2() {
-		return operand2;
-	}
+    public Expression getOperand1() {
+        return operand1;
+    }
 
-	public Operator getOperator() {
-		return operator;
-	}
+    public Expression getOperand2() {
+        return operand2;
+    }
 
-	public int getTotalOperatorCount() {
-		return totalOperatorCount;
-	}
+    public Operator getOperator() {
+        return operator;
+    }
 
-	public BigFraction getValue() {
-		return value;
-	}
+    public int getTotalOperatorCount() {
+        return totalOperatorCount;
+    }
 
-	public boolean isSimple() {
-		return simple;
-	}
+    public BigFraction getValue() {
+        return value;
+    }
 
-	@Override
-	public String toString() {
-		return toString(false);
-	}
+    public boolean isSimple() {
+        return simple;
+    }
 
-	public String toString(boolean fullParenthesis) {
-		final String SPACE = " ";
-		final String OPEN_PARENTHESIS = "(";
-		final String CLOSE_PARENTHESIS = ")";
+    @Override
+    public String toString() {
+        return toString(false);
+    }
 
-		StringBuilder sb = new StringBuilder();
-		if (operator != null) {
-			// we have to use parantheses to guarantee the lower operators
-			// calculates first
-			// (a + b) * c
-			if (!operand1.simple && (fullParenthesis || (operand1.operator.getPriority() < operator.getPriority()))) {
-				sb.append(OPEN_PARENTHESIS).append(operand1.toString(fullParenthesis)).append(CLOSE_PARENTHESIS);
-			} else {
-				sb.append(operand1.toString(fullParenthesis));
-			}
-			sb.append(SPACE).append(operator).append(SPACE);
+    public String toString(boolean fullParenthesis) {
+        final String SPACE = " ";
+        final String OPEN_PARENTHESIS = "(";
+        final String CLOSE_PARENTHESIS = ")";
 
-			// guarantee the lower operators calculates first too, plus another
-			// case: a - (b + c) or a / (b / c) but not a - b * c
-			if (!operand2.simple
-					&& (fullParenthesis || ((operand2.operator.getPriority() < operator.getPriority() || (!operator
-							.isExchangable() && operator.getPriority() == operand2.operator.getPriority()))))) {
-				sb.append(OPEN_PARENTHESIS).append(operand2.toString(fullParenthesis)).append(CLOSE_PARENTHESIS);
-			} else {
-				sb.append(operand2.toString(fullParenthesis));
-			}
-		} else {
-			if (value.getDenominator().intValue() == 1) {
-				sb.append(value.getNumerator());
-			} else {
-				sb.append(OPEN_PARENTHESIS).append(value.toString()).append(CLOSE_PARENTHESIS);
-			}
-		}
-		return sb.toString();
-	}
+        StringBuilder sb = new StringBuilder();
+        if (operator != null) {
+            // we have to use parantheses to guarantee the lower operators
+            // calculates first
+            // (a + b) * c
+            if (!operand1.simple
+                    && (fullParenthesis || (operand1.operator.getPriority() < operator
+                            .getPriority()))) {
+                sb.append(OPEN_PARENTHESIS).append(operand1.toString(fullParenthesis))
+                        .append(CLOSE_PARENTHESIS);
+            } else {
+                sb.append(operand1.toString(fullParenthesis));
+            }
+            sb.append(SPACE).append(operator).append(SPACE);
+
+            // guarantee the lower operators calculates first too, plus another
+            // case: a - (b + c) or a / (b / c) but not a - b * c
+            if (!operand2.simple
+                    && (fullParenthesis || ((operand2.operator.getPriority() < operator
+                            .getPriority() || (!operator.isExchangable() && operator.getPriority() == operand2.operator
+                            .getPriority()))))) {
+                sb.append(OPEN_PARENTHESIS).append(operand2.toString(fullParenthesis))
+                        .append(CLOSE_PARENTHESIS);
+            } else {
+                sb.append(operand2.toString(fullParenthesis));
+            }
+        } else {
+            if (value.getDenominator().intValue() == 1) {
+                sb.append(value.getNumerator());
+            } else {
+                sb.append(OPEN_PARENTHESIS).append(value.toString()).append(CLOSE_PARENTHESIS);
+            }
+        }
+        return sb.toString();
+    }
 }
